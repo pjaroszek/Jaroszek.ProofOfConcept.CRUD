da0428682b8f464474e31d9b9f4c572526541004
Paweł Jaroszek <pawel.jaroszek@digit-al.pl>
2019-10-26 21:31:36 +0200

    Implement crud

---
+++ Jaroszek.ProofOfConcept.CRUD/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace Jaroszek.ProofOfConcept.CRUD
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string Description { get; set; }
+    }
+}
--- Jaroszek.ProofOfConcept.CRUD/Program.cs
+++ Jaroszek.ProofOfConcept.CRUD/Program.cs
@@ -1,15 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Jaroszek.ProofOfConcept.CRUD.Services;
 
 namespace Jaroszek.ProofOfConcept.CRUD
 {
     class Program
     {
+        public static List<Person> Persons = new List<Person>();
         static void Main(string[] args)
         {
+            CrudOperations crud = new CrudOperations();
+            crud.Insert();
+
+            Person person = new Person();
+
+            person.Id = 1;
+            person.Name = "Pawel";
+            person.Surname = "Nowak";
+            person.Street = "Rejtana";
+            person.Number = "22";
+            person.PostalCode = "35-103";
+            person.City = "Rzeszow";
+            person.Description = "Opis ";
+
+
+            crud.Update(person);
+
+
+            crud.Remove(2);
+
+
+
         }
     }
 }
