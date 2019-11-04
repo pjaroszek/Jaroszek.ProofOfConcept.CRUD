2e2301156d6c59dcd1f567e39056e5c5e5f4ff70
Paweł Jaroszek <pjaroszek@gmail.com>
2019-11-04 21:05:09 +0100

    add methot to operation in SQL database

---
+++ Jaroszek.ProofOfConcept.SqlServer/Services/OperationRepositorySQL.cs
@@ -0,0 +1,54 @@
+﻿using Jaroszek.ProofOfConcept.DataAccess.Models;
+using Jaroszek.ProofOfConcept.SqlServer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Jaroszek.ProofOfConcept.SqlServer.Services
+{
+    public class OperationRepositorySql : IPersonRepository
+    {
+        private List<Person> _persons = new List<Person>();
+
+        public List<Person> GetAll()
+        {
+            return _persons;
+        }
+
+        public Person GetByName(string name)
+        {
+            return _persons.Where(p => p.Name == name).SingleOrDefault();
+        }
+
+        public void AddNew(PersonDto personDto)
+        {
+
+            Person person = new Person();
+
+            person.Id = 1;
+            person.Name = personDto.Name;
+            person.Surname = "Nowak";
+            person.Street = "Rejtana";
+            person.Number = "19";
+            person.PostalCode = "35-103";
+            person.City = "Rzeszów";
+            person.AddDate = DateTime.Now;
+            person.Description = personDto.Description;
+
+            _persons.Add(person);
+
+        }
+
+        public void Update(PersonDto person)
+        {
+            Person p = _persons.Where(x => x.Name == person.Name).SingleOrDefault();
+            p.Description = person.Description;
+
+        }
+
+        public void DeleteById(int id)
+        {
+            _persons.Remove(_persons.Where(p => p.Id == id).SingleOrDefault());
+        }
+    }
+}
